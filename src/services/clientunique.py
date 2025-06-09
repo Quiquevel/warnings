4844061fd83682becefa81480250b12d7bd2c42f
Quiquevel <98911999+Quiquevel@users.noreply.github.com>
2025-06-09 16:18:28 +0200

    Update clientunique.py

--- src/services/clientunique.py
+++ src/services/clientunique.py
@@ -1,8 +1,9 @@
-import os
+'''This module defines the WarningModel class used for handling warning data in the application.'''
+from os import getenv
 from shuttlelib.openshift.client import OpenshiftClient
 from shuttlelib.utils.logger import logger
 
-entity_id = os.getenv("ENTITY_ID", "spain")
+entity_id = getenv("ENTITY_ID", "spain")
 if entity_id is None:
     logger.error("ENTITY_ID environment variable is not set")
     raise ValueError("ENTITY_ID environment variable is not set")
@@ -45,3 +46,35 @@ def getenvironmentsclusterslist():
     except Exception as e:
         logger.error(f"Error in getenvironmentsclusterslist: {str(e)}", exc_info=True)
         return [], {}, {}
+    
+def getenvironmentsclustersregionslist():
+    '''Get the list of environments, clusters, and regions from OpenshiftClient, including upper case and special values.'''
+    environmentlist = []
+    clusterlist = []
+    regionlist = []
+    clusterlistaux = []
+    regionlistaux = []
+
+    environmentlist = list(client.clusters.keys())
+
+    for environment in environmentlist:
+        clusterlistaux = list(client.clusters[environment].keys())
+        for cluster in clusterlistaux:
+            regionlistaux = list(client.clusters[environment][cluster].keys())
+            regionlist.extend(regionlistaux)
+        clusterlist.extend(clusterlistaux)
+
+    environmentlist.extend([x.upper() for x in environmentlist])
+    environmentlist.extend(["all", "ALL"])
+
+    clusterlist.extend([x.upper() for x in clusterlist])
+    clusterlist.extend(["all", "ALL"])
+    clusterlist = list(set(clusterlist))
+    clusterlist.sort()
+
+    regionlist.extend([x.upper() for x in regionlist])
+    regionlist.extend(["both", "BOTH"])
+    regionlist = list(set(regionlist))
+    regionlist.sort()
+
+    return environmentlist, clusterlist, regionlist
